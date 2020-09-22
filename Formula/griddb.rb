27036a3e18dba68f13316f0d9e0beee45950f29c
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-22 18:49:41 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -1,9 +1,10 @@
 class Griddb < Formula
-  desc "Internet file retriever"
+  desc "Nguyen Tien Tung Duong"
   homepage "https://github.com/tungduong97/homebrew-tools"
-  url "https://github.com/tungduong97/homebrew-tools/files/5049101/tungduong.tar.gz"
+  #url "https://github.com/tungduong97/homebrew-tools/files/5049101/tungduong.tar.gz"
+  url "https://github.com/tungduong97/homebrew-tools/files/5261144/nttd.tar.gz" 
   version "1.0.0"
-  sha256 "236c275a317947bb4595ac50cfc52d54ebc875ab0e5cc083f0196d921af80108"
+  sha256 "4340de315795d3ef57e8423170245e066d1150f75ebffe523ec1533976ad9d2c"
 
   depends_on "autoconf"
   depends_on "automake"
