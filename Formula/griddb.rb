dc7d7244e511c640a417527f7daa7b3bf2546d30
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-23 10:15:24 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -4,7 +4,13 @@ class Griddb < Formula
   url "https://github.com/tungduong97/homebrew-tools/files/5261144/nttd.tar.gz"
   version "1.0.0"
   sha256 "4340de315795d3ef57e8423170245e066d1150f75ebffe523ec1533976ad9d2c"
-  #license "YMH"
+  license "YMH"
+  
+  bottle do
+    sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
+    sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
+    sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
+  end
 
   depends_on "autoconf"
   depends_on "automake"
@@ -16,7 +22,7 @@ class Griddb < Formula
   end
 
   test do
+    assert_equal "#{HOMEBREW_PREFIX}/lib", shell_output("#{bin}/pg_config --libdir").chomp
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
