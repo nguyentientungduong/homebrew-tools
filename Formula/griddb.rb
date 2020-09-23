4fe28cdc376f35c8068952990ccfe99e6a6d9aa4
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-23 14:39:16 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -7,7 +7,7 @@ class Griddb < Formula
   license "YMH"
   
   bottle do
-    #sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
+    sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
     sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
     sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
   end
@@ -22,7 +22,7 @@ class Griddb < Formula
   end
 
   test do
-    assert_equal "#{HOMEBREW_PREFIX}/lib", shell_output("#{bin}/pg_config --libdir").chomp
+    system "false"
     # executables being tested: `system "#{bin}/program", "do", "something"`.
   end
 end
