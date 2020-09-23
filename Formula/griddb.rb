e8b678694847c15117a71e522dfb0ef8a9860f77
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-23 13:46:51 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -7,9 +7,11 @@ class Griddb < Formula
   license "YMH"
   
   bottle do
+    cellar :any_skip_relocation
+    rebuild 1
     sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
-    sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
-    sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
+    #sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
+    #sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
   end
 
   depends_on "autoconf"
@@ -18,7 +20,10 @@ class Griddb < Formula
   depends_on "llvm"
 
   def install
-    system "cd client/c; ./bootstrap.sh; ./configure --prefix=#{prefix} ; make install"
+    system "Dir client/c"
+    system "./bootstrap.sh"
+    system "configure --prefix=#{prefix}"
+    system "make", " install"
   end
 
   test do
