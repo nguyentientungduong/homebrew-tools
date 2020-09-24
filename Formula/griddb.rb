e1ab4b6cfc1d20024a6d02301e3c6fdbde3aa929
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-24 09:07:00 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -6,11 +6,11 @@ class Griddb < Formula
   sha256 "4340de315795d3ef57e8423170245e066d1150f75ebffe523ec1533976ad9d2c"
   license "YMH"
   
-  bottle do
-    sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
-    sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
-    sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
-  end
+  #bottle do
+    #sha256 "cf7e64a4a702db6667582d6a8d83107b67d1457a623fbe04426d9702023612fa" => :catalina
+    #sha256 "199d0fa0c97e376933c8cca82e44503b36f610f7edb2170e6a1e7e7491c82017" => :mojave
+    #sha256 "c210ec1143b73616108891c3cefaa31754c5bd5fda0222e2e3266035488a1353" => :high_sierra
+  #end
 
   depends_on "autoconf"
   depends_on "automake"
@@ -24,6 +24,7 @@ class Griddb < Formula
   end
 
   test do
-    system "gcc #{prefix}/sample1.c -lgridstore -o #{prefix}/sample1; #{prefix}/sample1"
+    system "gcc", "#{prefix}/sample1.c", "-lgridstore", "-o", "sample1"
+    assert_equal "Hello, world!\n", `./sample1`
   end
 end
