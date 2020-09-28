77f7c5a94848bb93a87322da60047e1c9fcea3ba
Nguyen Tien Tung Duong <49387566+tungduong97@users.noreply.github.com>
2020-09-28 14:15:41 +0700

    Update griddb-c-client.rb

--- Formula/griddb-c-client.rb
+++ Formula/griddb-c-client.rb
@@ -6,7 +6,7 @@ class GriddbCClient < Formula
   sha256 "bf9eaca4df14bd3badc662dc3a6db5cdcae5b35e9e50b48427c9b9dd02bc116e"
   license "Apache-2.0"
 
-  bottle :unneeded
+  #bottle :unneeded
 
   depends_on "autoconf"
   depends_on "automake"
