e0ff78b76a8f3e6d56cfdc50bb5a2149cff29322
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2021-01-13 16:29:42 +0700

    Update griddb-c-client.rb

--- Formula/griddb-c-client.rb
+++ Formula/griddb-c-client.rb
@@ -30,7 +30,7 @@ class GriddbCClient < Formula
         return 0;
       }
     EOS
-    system `#{ENV.cc}`, "-I#{include}", "-L#{lib}", "sample.c", "-lgridstore", "-o", "sample"
+    system `"#{ENV.cc}", "-I#{include}", "-L#{lib}", "sample.c", "-lgridstore", "-o", "sample"`
     assert_equal "Get default GSGridStoreFactory instance", `./sample`
   end
 end
