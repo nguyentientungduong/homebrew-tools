0fccd413667a37ee4d4e0e59d741a3b4f0fc945e
Nguyen Tien Tung Duong <49387566+nguyentientungduong@users.noreply.github.com>
2022-03-23 09:14:54 +0700

    Update griddb-c-client.rb

--- Formula/griddb-c-client.rb
+++ Formula/griddb-c-client.rb
@@ -9,33 +9,27 @@ class GriddbCClient < Formula
   depends_on "autoconf"
   depends_on "automake"
   depends_on "libtool"
-  uses_from_macos "llvm"
+  depends_on "llvm"
 
   def install
-    cd "client/c" do
-      touch "AUTHORS", "COPYING", "ChangeLog", "INSTALL", "NEWS", "README"
-      system "glibtoolize", "-c"
-      system "aclocal"
-      system "autoconf"
-      system "automake", "-a", "-c"
-      system "./configure", "--prefix=#{prefix}"
-      system "make", "install"
-    end
+    Dir.chdir('client/c')
+    system "./bootstrap.sh"
+    system "./configure --prefix=#{prefix}"
+    system "make", "install"
   end
 
   test do
     (testpath/"sample.c").write <<~EOS
       #include "gridstore.h"
       #include <stdio.h>
-      int main() {
-          GSGridStoreFactory* p = gsGetDefaultFactory();
-          if (p != NULL) {
-              printf("Get default GSGridStoreFactory instance");
-          }
-          return 0;
+      int main()
+      {
+        GSGridStoreFactory* p = gsGetDefaultFactory();
+        if (p != NULL) printf("Get default GSGridStoreFactory instance");
+        return 0;
       }
     EOS
-    system ENV["CC"], "-I#{include}", "-L#{lib}", "sample.c", "-lgridstore", "-o", "sample"
+    system "gcc", "-I#{include}", "-L#{lib}", "sample.c", "-lgridstore", "-o", "sample"
     assert_equal "Get default GSGridStoreFactory instance", `./sample`
   end
 end
